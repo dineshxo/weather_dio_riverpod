6af0a9de68c592d22eb53c2036616b5d8f8b200c
Dinesh Manchanayaka <mail.dineshmadusanka@gmail.com>
2024-09-12 10:03:52 +0530

    update : separate dependencies

--- lib/providers/current_weather_provider.dart
+++ lib/providers/current_weather_provider.dart
@@ -1,7 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_dio_riverpod/models/current_weather.dart';
 import 'package:weather_dio_riverpod/services/weather_services.dart';
+import 'package:dio/dio.dart';
 
-final currentWeatherProvider = FutureProvider.family<CurrentWeather,String>((ref,String city){
-  return WeatherServices.fetchWeather(city);
+
+final dioProvider = Provider<Dio>((ref) {
+  return Dio();
+});
+
+final weatherServicesProvider = Provider<WeatherServices>((ref) {
+  final dio = ref.read(dioProvider);
+  return WeatherServices(dio: dio);
+});
+
+final currentWeatherProvider = FutureProvider.family<CurrentWeather, String>((ref, String city) {
+  final weatherService = ref.read(weatherServicesProvider);
+  return weatherService.fetchWeather(city);
 });
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -5,13 +5,11 @@ import 'package:weather_dio_riverpod/models/current_weather.dart';
 
 
 class WeatherServices {
-
-  // static const weatherLatLonURL = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5058ed6e6d06bf91b6ef2ef25d80526e';
-
-  static final Dio dio =Dio();
+  WeatherServices({required this.dio});
+   final Dio dio;
 
 
-  static Future<CurrentWeather> fetchWeather(String city)async{
+   Future<CurrentWeather> fetchWeather(String city)async{
      final String weatherCityURL= 'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$APIKEY';
      try {
        final response = await dio.get(weatherCityURL);
@@ -28,3 +26,6 @@ class WeatherServices {
      }
   }
 }
+
+
+// static const weatherLatLonURL = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5058ed6e6d06bf91b6ef2ef25d80526e';
